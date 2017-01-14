c6c79aa322f37d5c394ed4030d24b3c06205e753
Bokac <Bokac@Prometheus>
2017-01-14 21:11:17 +0100

    nove popravke za email

--- app/Config/email.php
+++ app/Config/email.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
@@ -36,56 +37,56 @@
  */
 class EmailConfig {
 
-	public $default = array(
-		'transport' => 'Mail',
-		'from' => 'contact@tibih-database.org',
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
-	public $smtp = array(
-		'transport' => 'Smtp',
-		'from' => array('site@localhost' => 'My Site'),
-		'host' => 'localhost',
-		'port' => 465,
-		'timeout' => 30,
-		'username' => 'contact@tibih-database.org',
-		'password' => '2bRXhEMfdwcI',
-		'client' => null,
-		'log' => false,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
-
-	public $fast = array(
-		'from' => 'you@localhost',
-		'sender' => null,
-		'to' => null,
-		'cc' => null,
-		'bcc' => null,
-		'replyTo' => null,
-		'readReceipt' => null,
-		'returnPath' => null,
-		'messageId' => true,
-		'subject' => null,
-		'message' => null,
-		'headers' => null,
-		'viewRender' => null,
-		'template' => false,
-		'layout' => false,
-		'viewVars' => null,
-		'attachments' => null,
-		'emailFormat' => null,
-		'transport' => 'Smtp',
-		'host' => 'localhost',
-		'port' => 25,
-		'timeout' => 30,
-		'username' => 'user',
-		'password' => 'secret',
-		'client' => null,
-		'log' => true,
-		//'charset' => 'utf-8',
-		//'headerCharset' => 'utf-8',
-	);
+    public $default = array(
+        'transport' => 'Mail',
+        'from' => array('contact@tibih-database.org' => 'Arhiva TIBIH'),
+        'replyTo' => 'contact@tibih-database.org',
+        'charset' => 'utf-8',
+        'headerCharset' => 'utf-8',
+        'headers' => array('X-Mailer' => 'Arhiva TIBIH'),        
+    );
+    public $smtp = array(
+        'transport' => 'Smtp',
+        'from' => array('site@localhost' => 'My Site'),
+        'host' => 'localhost',
+        'port' => 465,
+        'timeout' => 30,
+        'username' => 'contact@tibih-database.org',
+        'password' => '2bRXhEMfdwcI',
+        'client' => null,
+        'log' => false,
+            //'charset' => 'utf-8',
+            //'headerCharset' => 'utf-8',
+    );
+    public $fast = array(
+        'from' => 'you@localhost',
+        'sender' => null,
+        'to' => null,
+        'cc' => null,
+        'bcc' => null,
+        'replyTo' => null,
+        'readReceipt' => null,
+        'returnPath' => null,
+        'messageId' => true,
+        'subject' => null,
+        'message' => null,
+        'headers' => null,
+        'viewRender' => null,
+        'template' => false,
+        'layout' => false,
+        'viewVars' => null,
+        'attachments' => null,
+        'emailFormat' => null,
+        'transport' => 'Smtp',
+        'host' => 'localhost',
+        'port' => 25,
+        'timeout' => 30,
+        'username' => 'user',
+        'password' => 'secret',
+        'client' => null,
+        'log' => true,
+            //'charset' => 'utf-8',
+            //'headerCharset' => 'utf-8',
+    );
 
 }
