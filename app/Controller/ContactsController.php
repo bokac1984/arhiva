2a31ea0798abde3fa5d740c4d38418b391815775
Bokac <Bokac@Prometheus>
2017-01-14 21:12:01 +0100

    nove popravke za email

--- app/Controller/ContactsController.php
+++ app/Controller/ContactsController.php
@@ -12,10 +12,7 @@ class ContactsController extends AppController {
     
     public function index() {
         $Email = new CakeEmail();
-        $Email->from(array('contact@tibih-database.org' => 'Arhiva TIBIH'));
         $Email->to('bokac1984@gmail.com');
-        $Email->subject('Kontakt sa Arhive');
-        $Email->replyTo('contact@tibih-database.org');
         $Email->send('Poruka sa kojom pokusavamo');
     }
 }
