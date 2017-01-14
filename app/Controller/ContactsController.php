516fb709ad31582723469609e287323c930d224d
Bokac <Bokac@Prometheus>
2017-01-14 22:07:57 +0100

    popravio sam contacts

--- app/Controller/ContactsController.php
+++ app/Controller/ContactsController.php
@@ -2,18 +2,133 @@
 
 App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
-
+// secret Key: 6LcR2hEUAAAAAGLbxJx0g5oHn31YaiTLJHnVIDi5
 /**
  * Contacts Controller
  *
+ * @property Contact $Contact
  * @property PaginatorComponent $Paginator
  */
 class ContactsController extends AppController {
-    
-    public function index() {
-        $Email = new CakeEmail();
-        $Email->to('bokac1984@gmail.com');
-        $Email->send('Poruka sa kojom pokusavamo');
-    }
-}
 
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
+
+    private function checkCaptcha($url) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16");
+        $curlData = curl_exec($curl);
+        curl_close($curl);
+        return $curlData;       
+    }
+    
+    public function beforeFilter() {
+        parent::beforeFilter();
+        // Allow users to register and logout.
+        $this->Auth->allow('add');
+    }
+    
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->Contact->recursive = 0;
+        $this->set('contacts', $this->Paginator->paginate());
+    }
+
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->Contact->exists($id)) {
+            throw new NotFoundException(__('Invalid contact'));
+        }
+        $options = array('conditions' => array('Contact.' . $this->Contact->primaryKey => $id));
+        $this->set('contact', $this->Contact->find('first', $options));
+    }
+
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $google_url = "https://www.google.com/recaptcha/api/siteverify";
+            $secret = '6LcR2hEUAAAAAGLbxJx0g5oHn31YaiTLJHnVIDi5';
+            $ip = $this->request->clientIp();
+            $url = $google_url . "?secret=" . $secret . "&response=" . $recaptcha ."&remoteip=" . $ip;
+            $r = $this->checkCaptcha($url);
+            $res = json_decode($r, true);
+            
+            if (!empty($res)) {
+                $this->Contact->create();
+                if ($this->Contact->save($this->request->data)) {
+                    $this->Flash->success(__('The contact has been saved.'));
+                    return $this->redirect(array('action' => 'index'));
+                } else {
+                    $this->Flash->error(__('The contact could not be saved. Please, try again.'));
+                }  
+            }
+        }
+    }
+
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($id = null) {
+        if (!$this->Contact->exists($id)) {
+            throw new NotFoundException(__('Invalid contact'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->Contact->save($this->request->data)) {
+                $this->Flash->success(__('The contact has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Flash->error(__('The contact could not be saved. Please, try again.'));
+            }
+        } else {
+            $options = array('conditions' => array('Contact.' . $this->Contact->primaryKey => $id));
+            $this->request->data = $this->Contact->find('first', $options);
+        }
+    }
+
+    /**
+     * delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        $this->Contact->id = $id;
+        if (!$this->Contact->exists()) {
+            throw new NotFoundException(__('Invalid contact'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->Contact->delete()) {
+            $this->Flash->success(__('The contact has been deleted.'));
+        } else {
+            $this->Flash->error(__('The contact could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
+}
