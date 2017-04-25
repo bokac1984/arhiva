c869e8a1a80ea20136002ea0678468ab2b4392f0
Bokac <Bokac@Prometheus>
2017-04-25 21:35:38 +0200

    radnomizirao

--- app/Controller/Component/ManipulateComponent.php
+++ app/Controller/Component/ManipulateComponent.php
@@ -63,7 +63,7 @@ class ManipulateComponent extends Component {
             /**
              * ubaci neki radnom na osnovu vremena
              */
-            $now = $this->random_str(10);
+            $now = $this->generateRandomString();
             $temporalNameHashed = hash('sha512', $originalName . $now);
             $temporalName = substr($temporalNameHashed, 0, 10);
         }
@@ -119,26 +119,15 @@ class ManipulateComponent extends Component {
         return $this->copyFile($digitalFolder->path, $path, $contractName);
     }
     
-    /**
-     * Generate a random string, using a cryptographically secure 
-     * pseudorandom number generator (random_int)
-     * 
-     * For PHP 7, random_int is a PHP core function
-     * For PHP 5.x, depends on https://github.com/paragonie/random_compat
-     * 
-     * @param int $length      How many characters do we want?
-     * @param string $keyspace A string of all possible characters
-     *                         to select from
-     * @return string
-     */
-    public function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    {
-        $str = '';
-        $max = mb_strlen($keyspace, '8bit') - 1;
-        for ($i = 0; $i < $length; ++$i) {
-            $str .= $keyspace[random_int(0, $max)];
+    
+    public function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
         }
-        return $str;
-    }    
+        return $randomString;
+    }   
 
 }
