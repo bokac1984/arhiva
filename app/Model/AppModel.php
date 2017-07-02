74d31b4dc91fc69014e3f8e9883c1f95854ee758
Bokac <Bokac@Prometheus>
2017-07-02 22:04:15 +0200

    debugovanje

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -76,6 +76,7 @@ class AppModel extends Model {
         );
 
         $this->create();
+        debug($dataToSave);
         if ($this->save($dataToSave)) {
             return $this->getLastInsertID();
         } else {
