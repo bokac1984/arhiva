04ce2c010abd3953edc466260330a054da9ede70
Bokac <Bokac@Prometheus>
2017-07-04 22:04:48 +0200

    dodao metodu za check and save

--- app/Model/AgreementType.php
+++ app/Model/AgreementType.php
@@ -42,4 +42,40 @@ class AgreementType extends AppModel {
         )
     );
 
+    public function checkAndSave($name = '') {
+        $exist = $this->find('all', array(
+            'conditions' => array(
+                'OR' => array(
+                    "UPPER({$this->alias}.name)" => mb_strtoupper($name, 'UTF-8'),
+                    "{$this->alias}.name" => $name,
+                    "LCASE({$this->alias}.name)" => mb_strtolower($name, 'UTF-8'),
+
+                ),
+            ),
+            'fields' => array(
+                "{$this->alias}.id"
+            )
+        ));
+
+        if (count($exist) === 1) {
+            return $exist[0][$this->alias]['id'];
+        } else if (count($exist) > 1) {
+            return 0;
+        }
+
+        $dataToSave = array(
+            "{$this->alias}" => array(
+                'name' => $name
+            )
+        );
+
+        $this->create();
+        
+        if ($this->save($dataToSave)) {
+            return $this->getLastInsertID();
+        }
+
+        return 0;
+    }
+
 }
